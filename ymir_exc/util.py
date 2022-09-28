a0dc17beedf144bfd4acc1ea23a9d2c4e7b5ff69
youdaoyzbx <youdaoyzbx@163.com>
2022-09-28 13:53:34 +0800

    make preprocess 0.0001

--- ymir_exc/util.py
+++ ymir_exc/util.py
@@ -40,17 +40,18 @@ class YmirStageWeight(object):
         """
         weights: weight for each ymir stage
         if weights is None:
-            self.weights[0] = float(os.getenv('PREPROCESS_WEIGHT', 0.1))
-            self.weights[1] = float(os.getenv('TASK_WEIGHT', 0.8))
-            self.weights[2] = float(os.getenv('POSTPROCESS_WEIGHT', 0.1))
+            self.weights = [0, 0, 0]
+            self.weights[0] = float(os.getenv('PREPROCESS_WEIGHT', 0.0001))
+            self.weights[1] = float(os.getenv('TASK_WEIGHT', 0.9998))
+            self.weights[2] = float(os.getenv('POSTPROCESS_WEIGHT', 0.0001))
         """
         if weights:
             self.weights = weights
         else:
             self.weights = [0, 0, 0]
-            self.weights[0] = float(os.getenv('PREPROCESS_WEIGHT', 0.1))
-            self.weights[1] = float(os.getenv('TASK_WEIGHT', 0.8))
-            self.weights[2] = float(os.getenv('POSTPROCESS_WEIGHT', 0.1))
+            self.weights[0] = float(os.getenv('PREPROCESS_WEIGHT', 0.0001))
+            self.weights[1] = float(os.getenv('TASK_WEIGHT', 0.9998))
+            self.weights[2] = float(os.getenv('POSTPROCESS_WEIGHT', 0.0001))
 
         assert sum(self.weights) == 1, f'sum of weights {weights} != 1'
         assert len(self.weights) == 3, f'len of weights {weights} != 3'
