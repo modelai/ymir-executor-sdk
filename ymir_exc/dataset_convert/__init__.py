5292d099c4a4d619742fdf168179c2fb6b95f1c0
youdaoyzbx <youdaoyzbx@163.com>
2022-11-02 14:09:19 +0800

    fix F401

--- ymir_exc/dataset_convert/__init__.py
+++ ymir_exc/dataset_convert/__init__.py
@@ -1,2 +1,4 @@
-from .ymir2yolov5 import convert_ymir_to_yolov5
-from .ymir2coco import convert_ymir_to_coco
+from .ymir2yolov5 import convert_ymir_to_yolov5  # noqa
+from .ymir2coco import convert_ymir_to_coco  # noqa
+
+__all__ = ['convert_ymir_to_yolov5', 'convert_ymir_to_coco']
