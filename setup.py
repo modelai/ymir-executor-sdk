ec7a596572d8c0b0a506711e1d92783ea4263ce5
youdaoyzbx <youdaoyzbx@163.com>
2023-01-05 10:26:22 +0800

    remove additional function

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-__version__ = "2.1.0.1222"
+__version__ = "2.1.1.0105"
 
 requirements = []
 with open("requirements.txt") as f:
--- ymir_exc/dataset_convert/__init__.py
+++ ymir_exc/dataset_convert/__init__.py
@@ -1,10 +1,9 @@
 from .ymir2coco import convert_ymir_to_coco  # noqa
-from .ymir2mmseg import convert_rgb_to_label_id, convert_ymir_to_mmseg  # noqa
+from .ymir2mmseg import find_blank_area_in_dataset  # noqa
 from .ymir2yolov5 import convert_ymir_to_yolov5  # noqa
 
 __all__ = [
     "convert_ymir_to_yolov5",
     "convert_ymir_to_coco",
-    "convert_rgb_to_label_id",
-    "convert_ymir_to_mmseg",
+    "find_blank_area_in_dataset",
 ]
